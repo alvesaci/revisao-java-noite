e9333cc9d13831ae7fa5f5902b23ba0805a94f3f
MANOEL ALVES VIANA JUNIOR <a9224054@rn.senac.br>
2018-07-26 21:20:09 -0300

    Aula-26/07/18

---
+++ src/exemplo2/TestaContaCorrente.java
@@ -0,0 +1,36 @@
+package exemplo2;
+
+public class TestaContaCorrente {
+
+	public static void main(String[] args) {
+		Cliente natan = new Cliente("Natan", "111.222.333-12", 25);
+		ContaCorrente contaNatan = new ContaCorrente();
+		contaNatan.setCliente(natan);
+		contaNatan.setAgencia("1234-5");
+		contaNatan.setNumero("123-2");
+		contaNatan.setLimite(1000f);
+		contaNatan.setSaldo(2000f);
+		
+			
+		
+		
+		Cliente jackson = new Cliente("jackson", "121.232.353-12", 43);
+		ContaCorrente contaJackson = new ContaCorrente();
+		contaJackson.setCliente(jackson);
+		contaJackson.setAgencia("986-5");
+		contaJackson.setNumero("987-2");
+		contaJackson.setLimite(500f);
+		contaJackson.setSaldo(3000f);
+		
+		//contaNatan.deposita(600f);
+		//contaJackson.saca(500f);
+		
+		contaJackson.transfere(500f, contaNatan);
+//		contaNatan.transfere(200f, conJackson);
+		
+		System.out.println(contaNatan);
+		System.out.println(contaJackson);
+
+	}
+
+}
